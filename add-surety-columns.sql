a9dd8fcc1a2c6a4a42c2f83c0cbad01f2e6de8f7
forchinthierry <forchinthierry@gmail.com>
2026-08-10 21:49:13 +0100

    agreement fix

---
+++ add-surety-columns.sql
@@ -0,0 +1,4 @@
+ALTER TABLE loan_applications ADD COLUMN surety_name TEXT;
+ALTER TABLE loan_applications ADD COLUMN surety_phone TEXT;
+ALTER TABLE loan_applications ADD COLUMN surety_address TEXT;
+ALTER TABLE loan_applications ADD COLUMN surety_relation TEXT;
